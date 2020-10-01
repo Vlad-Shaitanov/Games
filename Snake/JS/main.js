16ea98ddea3806b5e446a81409c034d2d15a605b
Vladislav <kelsenelenelvial@mail.ru>
2020-10-01 15:47:05 +0300

    Snake change img background

--- Snake/JS/main.js
+++ Snake/JS/main.js
@@ -1,32 +1,50 @@
 "use strict";
 
-let canvas = document.querySelector("#canvas"),
-	ctx = canvas.getContext("2d"),
-	background,
-	cell;
 
 let game = {
-	start() {
-		//Динамически создаем аналог тега <img>
-		background = new Image();
-		//Используя свойство, подключаем нужную картинку
-		background.src = "IMG/bg.png";
+	//присваиваем свойствам значения null, чтобы в будущем присвоить объекты
+	canvas: null,
+	ctx: null,
+	sprites: {
+		background: null,
+		cell: null,
+	},
 
-		cell = new Image();
-		cell.src = "IMG/cell.png";
-		//При полной загрузке картинки будет выполнена отрисовка на странице
-		background.addEventListener("load", function () {
+	start() {
+		this.canvas = document.querySelector("#canvas");
+		this.ctx = this.canvas.getContext("2d");
+
+		this.preload(() => {
 			this.run();
 		});
 	},
+
+	preload(callback) {
+		//Динамически создаем аналог тега <img>
+		this.sprites.background = new Image();
+		//Используя свойство, подключаем нужную картинку
+		this.sprites.background.src = "IMG/background.png";
+
+		this.sprites.cell = new Image();
+		this.sprites.cell.src = "IMG/cell.png";
+		//При полной загрузке картинки будет выполнена отрисовка на странице
+		this.background.addEventListener("load", () => {
+			/*такая запись сработает только со стрелочными функциями(ES6)
+			При передаче обычной функции контекст this будет другим
+			и вызов метода не сработает*/
+			this.run();
+		});
+		callback();
+	},
+
 	run() {
 		/*указываем браузеру на то, что нужно произвести анимацию, и просим его
 			запланировать перерисовку на следующем кадре анимации*/
-		window.requestAnimationFrame(function () {
+		window.requestAnimationFrame(() => {
 			/*Передаем картинку в контекст с координатами х=0 и у=0
 			началом оси координат считается верхний левый угол канваса.
 			*/
-			ctx.drawImage(background, 0, 0);
+			this.ctx.drawImage(this.background, 0, 0);
 		});
 	}
 };
