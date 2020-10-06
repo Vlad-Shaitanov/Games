0087b378e56db1959e24a80dbf69e771650bc96c
Vladislav <kelsenelenelvial@mail.ru>
2020-10-06 22:05:19 +0300

    upd

--- Breakout/gulpfile.js
+++ Breakout/gulpfile.js
@@ -13,7 +13,7 @@ let path = {//Содержит пути к файлам и папкам
 	},
 
 	src: {//Исходные файлы
-		html: sourceFolder + "/",
+		html: [sourceFolder + "/*.html", "!" + sourceFolder + "/_*.html"],
 		css: sourceFolder + "/scss/style.scss",
 		js: sourceFolder + "/js/main.js",
 		img: sourceFolder + "/img/**/*.{jpg,png,svg,gif,ico,webpp}",
@@ -34,5 +34,50 @@ let path = {//Содержит пути к файлам и папкам
 	clean: "./" + projectFolder + "/",//Очистка директории с готовым проектом
 };
 
-let { src, dist } = require("gulp"),
-	gulp = require("gulp");
+let { src, dest } = require("gulp"),
+	gulp = require("gulp"),
+	browsersync = require("browser-sync").create(),
+	fileinclude = require("gulp-file-include");
+
+function browserSync(param) {//Работа браузера
+	browsersync.init({//Инициализация обновления браузера
+		//Настройки плагина
+		server: {
+			//Базовая папка
+			baseDir: "./" + projectFolder + "/",
+			//Настройка порта
+			port: 3000,
+			//Отключение уведомления об обновл.браузера
+			notify: false,
+		},
+
+	});
+}
+
+function html() {//Обработка htmll
+	return src(path.src.html)
+		.pipe(fileinclude())//Объединение файлов в один
+		/*Создает поток для записи объектов в файловую систему.*/
+		.pipe(dest(path.build.html))//Путь к папке с результатом
+		.pipe(browsersync.stream());//Обновление страницы
+}
+
+function watchFiles(param) {//Следит за обновление файлов
+	gulp.watch([path.watch.html], html);//Слежка за html
+}
+
+/*Объединяет функции задач и/или составные операции в более крупные, которые
+выполняются последовательно. Нет никаких ограничений на глубину вложенности
+составных операций*/
+let build = gulp.series(html);
+
+/*Объединяет функции задач и/или составные операции в более крупные,
+которые выполняются одновременно (параллельно).
+Нет никаких ограничений на глубину вложенности составных операций*/
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+//При запуске Gulp будут выполняться эти переменные по умолчанию
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
