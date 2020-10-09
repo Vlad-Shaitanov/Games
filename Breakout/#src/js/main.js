30dbe0e79a003e6e3e802b3576cc81246a05e4a0
Vladislav <kelsenelenelvial@mail.ru>
2020-10-09 21:09:52 +0300

    game upd

--- Breakout/#src/js/main.js
+++ Breakout/#src/js/main.js
@@ -1,10 +1,50 @@
 "use strict";
 @@include("webp.js")
 
-let game = {
-	start() {
-		console.log("game started");
-	}
-};
 
-game.start();
+
+let game = {
+
+	ctx: null,
+	canvas: null,
+	background: null,
+	ball: null,
+
+	init() {
+		//Инициализация
+		this.canvas = document.querySelector("#canvas");
+		this.ctx = this.canvas.getContext("2d");
+	},
+
+	preload() {
+		//Предзагрузка спрайтов
+		this.background = new Image();
+		this.background.src = "img/background.png";
+		this.ball = new Image();
+		this.ball.src = "img/ball.png";
+	},
+
+	run() {
+		//Запуск
+		window.requestAnimationFrame(() => {
+			this.render();
+		});
+	},
+
+	render() {
+		//Отрисовка
+		console.log("render");
+		this.ctx.drawImage(this.background, 0, 0);
+		this.ctx.drawImage(this.ball, 0, 0);
+	},
+
+	start() {
+		this.init();
+		this.preload();
+		this.run();
+	},
+};
+window.addEventListener("load", () => {
+	//Старт игры только при полной загрузке структуры HTML
+	game.start();
+});
--- Breakout/dist/js/main.js
+++ Breakout/dist/js/main.js
@@ -1,27 +1,67 @@
 "use strict";
 //JS-функция определения поддержки WebP
-function testWebP(callback) {
+// function testWebP(callback) {
 
-	var webP = new Image();
-	webP.onload = webP.onerror = function () {
-		callback(webP.height == 2);
-	};
-	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
+// 	var webP = new Image();
+// 	webP.onload = webP.onerror = function () {
+// 		callback(webP.height == 2);
+// 	};
+// 	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+// }
 
-testWebP(function (support) {
+// testWebP(function (support) {
 
-	if (support == true) {
-		document.querySelector('body').classList.add('webp');
-	} else {
-		document.querySelector('body').classList.add('no-webp');
-	}
-});
+// 	if (support == true) {
+// 		document.querySelector('body').classList.add('webp');
+// 	} else {
+// 		document.querySelector('body').classList.add('no-webp');
+// 	}
+// });
+
+
 
 let game = {
-	start() {
-		console.log("game started");
-	}
-};
 
-game.start();
+	ctx: null,
+	canvas: null,
+	background: null,
+	ball: null,
+
+	init() {
+		//Инициализация
+		this.canvas = document.querySelector("#canvas");
+		this.ctx = this.canvas.getContext("2d");
+	},
+
+	preload() {
+		//Предзагрузка спрайтов
+		this.background = new Image();
+		this.background.src = "img/background.png";
+		this.ball = new Image();
+		this.ball.src = "img/ball.png";
+	},
+
+	run() {
+		//Запуск
+		window.requestAnimationFrame(() => {
+			this.render();
+		});
+	},
+
+	render() {
+		//Отрисовка
+		console.log("render");
+		this.ctx.drawImage(this.background, 0, 0);
+		this.ctx.drawImage(this.ball, 0, 0);
+	},
+
+	start() {
+		this.init();
+		this.preload();
+		this.run();
+	},
+};
+window.addEventListener("load", () => {
+	//Старт игры только при полной загрузке структуры HTML
+	game.start();
+});
