fcab93a59d7d3820e5e589abe41c51fd6bb8ee90
Vladislav <kelsenelenelvial@mail.ru>
2020-10-11 20:07:53 +0300

    drawing blocks

--- Breakout(arcanoid)/js/main.js
+++ Breakout(arcanoid)/js/main.js
@@ -4,10 +4,14 @@ let game = {
 	ctx: null,
 	platform: null,
 	ball: null,
+	blocks: [],
+	rows: 4,//Кол-во рядов с блоками
+	cols: 8,//Кол-во колонок с блоками
 	sprites: {
 		background: null,
 		ball: null,
 		platform: null,
+		block: null,
 	},
 
 	init() {
@@ -20,15 +24,29 @@ let game = {
 		//Предзагрузка файлов
 		let loaded = 0;
 		let required = Object.keys(this.sprites).length;
+
+		let onImageLoad = () => {
+			++loaded;
+			if (loaded >= required) {
+				callback();
+			}
+		};
+
 		for (let key in this.sprites) {
 			this.sprites[key] = new Image();
 			this.sprites[key].src = `img/${key}.png`;
-			this.sprites[key].addEventListener("load", () => {
-				++loaded;
-				if (loaded >= required) {
-					callback();
-				}
-			});
+			this.sprites[key].addEventListener("load", onImageLoad);
+		}
+	},
+
+	create() {//Создание группы блоков
+		for (let row = 0; row < this.rows; row++) {
+			for (let col = 0; col < this.cols; col++) {
+				this.blocks.push({
+					x: 64 * col + 65,
+					y: 24 * row + 35,
+				});
+			}
 		}
 	},
 
@@ -44,11 +62,19 @@ let game = {
 			this.ball.height, this.ball.x, this.ball.y,
 			this.ball.width, this.ball.height);//Отрисовка 1 кадра из спрайта
 		this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+		this.renderBlocks();
+	},
+
+	renderBlocks() {
+		for (let block of this.blocks) {//Отрисовка группы блоков
+			this.ctx.drawImage(this.sprites.block, block.x, block.y);
+		}
 	},
 
 	start() {
 		this.init();
 		this.preload(() => {
+			this.create();
 			//По факту загрузки preload, будет запущен метод run
 			this.run();
 		});
